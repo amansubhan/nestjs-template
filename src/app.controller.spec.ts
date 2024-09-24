e8590eab23cff10cb7cdd2e1ec1127022d046aa9
Aman Subhan <amansubhan929@gmail.com>
2024-09-24 16:13:32 +0200

    fix(test) fixed test
    Signed-off-by: Aman Subhan <aman@trooptravel.com>

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -15,8 +17,12 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      const result = await appController.getHello();
+      expect(result.message).toEqual(
+        expect.stringContaining('Hello World! from host'),
+      );
+      expect(typeof result.cacheViewCount).toBe('number');
     });
   });
 });
